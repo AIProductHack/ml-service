766431f7f52bcf70e2c1fed9f9e21575214fd69b
alimc <Alimchik.228@mail.ru>
2024-09-11 00:10:30 +0300

    Описание ваших изменений

--- src/generators/base.py
+++ src/generators/base.py
@@ -1,45 +1,15 @@
 import requests
 from abc import ABC, abstractmethod
 
-
 class GeneratorAPI(ABC):
     def __init__(self) -> None:
         super().__init__()
 
     @abstractmethod
-    def refresh_token(self, rid='85edd55c-2805-441a-a7f1-839485b58c58', secret='') -> None:
-        url = "https://ngw.devices.sberbank.ru:9443/api/v2/oauth"
+    def refresh_token(self) -> None:
+        pass
 
-        payload = 'scope=GIGACHAT_API_PERS'
-        headers = {
-            'Content-Type': 'application/x-www-form-urlencoded',
-            'Accept': 'application/json',
-            'RqUID': rid,
-            'Authorization': f'Basic {secret}'
-        }
-
-        response = requests.request("POST", url, headers=headers, data=payload, verify=False)
-        self.api_key = response.json()['access_token']
 
     @abstractmethod
-    def call_api(self, promt, quare) -> str:
-
-        url = "https://gigachat.devices.sberbank.ru/api/v1/chat/completions"
-
-        headers = {
-            "Content-Type": "application/json",
-
-            "Authorization": f"Bearer {self.api_key}"}
-
-        data = {
-            "model": "GigaChat:latest",
-            "messages": [
-                {"role": "system", "content": promt},
-                {"role": "user", "content": quare}
-            ],
-            "temperature": 0.7
-        }
-
-        response = requests.post(url, headers=headers, json=data, verify=False)
-        return response.json()['choices'][0]['message']['content']
-
+    def call_api(self) -> str:
+        pass
--- src/generators/gigachat.py
+++ src/generators/gigachat.py
@@ -7,6 +7,37 @@ class GigachatAPI(GeneratorAPI):
     def refresh_token(self) -> None:
         rid = os.getenv("GIGACHAT_RID")
         secret = os.getenv("GIGACHAT_SECRET")
+        url = "Y2JjZjcwMzMtZWFlYy00NWMxLTg0MTItMTFhMzliNjUwMTcwOmJmMWRmMTU0LTEzOWYtNGY5MC1hYjQ3LTIyMzY3NWFiMTQ3Zg=="
 
-    def call_api(self) -> str:
-        pass
+        payload = 'scope=GIGACHAT_API_PERS'
+        headers = {
+            'Content-Type': 'application/x-www-form-urlencoded',
+            'Accept': 'application/json',
+            'RqUID': rid,
+            'Authorization': f'Basic {secret}'
+        }
+
+        response = requests.request("POST", url, headers=headers, data=payload, verify=False)
+        self.api_key = response.json()['access_token']
+
+    def call_api(self, promt, quare) -> str:
+
+        url = "https://gigachat.devices.sberbank.ru/api/v1/chat/completions"
+
+        headers = {
+            "Content-Type": "application/json",
+
+            "Authorization": f"Bearer {self.api_key}"}
+
+        data = {
+            "model": "GigaChat:latest",
+            "messages": [
+                {"role": "system", "content": promt},
+                {"role": "user", "content": quare}
+            ],
+            "temperature": 0.7
+        }
+
+        response = requests.post(url, headers=headers, json=data, verify=False)
+        return response.json()['choices'][0]['message']['content']
+
